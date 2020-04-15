bad277500520d831061b566a6d0f389337db890d
phi dao <daovanphi25051994@gmail.com>
2020-04-15 19:13:50 +0700

    demo create simple thread

---
+++ src/TestThread.java
@@ -0,0 +1,15 @@
+public class TestThread {
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator1 = new NumberGenerator();
+        NumberGenerator numberGenerator2 = new NumberGenerator();
+        Thread thread1 = new Thread(numberGenerator1);
+        Thread thread2 = new Thread(numberGenerator2);
+        thread1.setName("A");
+        thread2.setName("B");
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+    }
+}
